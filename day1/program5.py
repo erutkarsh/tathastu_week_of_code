6c02faca22d6e509590c4f5223845dd63680fb01
erutkarsh <65822605+erutkarsh@users.noreply.github.com>
2020-05-23 22:15:32 +0530

    Create program5.py

---
+++ day1/program5.py
@@ -0,0 +1,15 @@
+scoreplayer1=int(input('Enter the score of first player in 60 balls'))
+scoreplayer2=int(input('Enter the score of second player in 60 balls'))
+scoreplayer3=int(input('Enter the score of third player in 60 balls'))
+strikerate1=scoreplayer1*100/60
+strikerate2=scoreplayer2*100/60
+strikerate3=scoreplayer3*100/60
+print("Strike rate of player 1 is",strikerate1)
+print("Strike rate of player 2 is",strikerate2)
+print("Strike rate of player 3 is",strikerate3)
+print("Runs scored by player 1 if they played 60 more deliveries:",scoreplayer1*2)
+print("Runs scored by player 2 if they played 60 more deliveries:",scoreplayer2*2)
+print("Runs scored by player 3 if they played 60 more deliveries:",scoreplayer3*2)
+print("Maxinum number of sixes player1 can hit:",scoreplayer1//6)
+print("Maxinum number of sixes player2 can hit:",scoreplayer2//6)
+print("Maxinum number of sixes player3 can hit:",scoreplayer3//6)
